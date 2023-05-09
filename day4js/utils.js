54a4f146b256e40f68c81703a286dc064d810b73
WIRYEFARMER <dusrb1418@naver.com>
2023-05-09 13:05:54 +0900

    day4 javascript first

---
+++ day4js/utils.js
@@ -0,0 +1,13 @@
+function hi() {
+    alert("Hi! This made from utils.js using with function hi()")
+
+}
+
+function sum(x,y) {
+    let result = x + y;
+    console.log(result);
+}
+
+function total(x,y) {
+    return x+y;
+}

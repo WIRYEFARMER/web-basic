9d4507d90d0fd12ec5260205310c43574b368bcc
WIRYEFARMER <dusrb1418@naver.com>
2023-05-16 09:14:56 +0900

    이미지파일을 절대경로를 상대경로로 변경

--- game/game.js
+++ game/game.js
@@ -1,8 +1,8 @@
-var character = document.getElementById('character');
-var obstacle = document.getElementById('obstacle');
-var bullet = document.getElementById('bullet');
-var gameOverDisplay = document.getElementById('game-over');
-var scoreDisplay = document.getElementById('score');
+var character = document.getElementById("character");
+var obstacle = document.getElementById("obstacle");
+var bullet = document.getElementById("bullet");
+var gameOverDisplay = document.getElementById("game-over");
+var scoreDisplay = document.getElementById("score");
 
 var score = 0;
 var jumping = false;
@@ -24,42 +24,42 @@ function jump() {
           clearInterval(fallInterval);
           jumping = false;
         } else {
-          character.style.bottom = --jumpCount + 'px';
+          character.style.bottom = --jumpCount + "px";
         }
       }, 0);
     } else {
-      character.style.bottom = ++jumpCount + 'px';
+      character.style.bottom = ++jumpCount + "px";
     }
   }, 2);
   //score+=100;
   //scoreDisplay.innerText = score;
 }
 
-document.addEventListener('keydown', function (event) {
-  if (event.code === 'Space') {
+document.addEventListener("keydown", function (event) {
+  if (event.code === "Space") {
     jump();
-  } else if (event.code === 'KeyZ' && !shooting && score >= 200) {
+  } else if (event.code === "KeyZ" && !shooting && score >= 200) {
     shooting = true;
     score -= 200;
     scoreDisplay.innerText = score;
     var bulletInterval = setInterval(function () {
       var bulletLeft = parseInt(
-        window.getComputedStyle(bullet).getPropertyValue('left')
+        window.getComputedStyle(bullet).getPropertyValue("left")
       );
       if (bulletLeft > window.innerWidth) {
         clearInterval(bulletInterval);
-        bullet.style.left = '-10px';
+        bullet.style.left = "-10px";
         shooting = false;
       } else {
-        bullet.style.left = bulletLeft + bulletSpeed + 'px';
+        bullet.style.left = bulletLeft + bulletSpeed + "px";
         var obstacleLeft = parseInt(
-          window.getComputedStyle(obstacle).getPropertyValue('left')
+          window.getComputedStyle(obstacle).getPropertyValue("left")
         );
         var obstacleBottom = parseInt(
-          window.getComputedStyle(obstacle).getPropertyValue('bottom')
+          window.getComputedStyle(obstacle).getPropertyValue("bottom")
         );
         var bulletBottom = parseInt(
-          window.getComputedStyle(bullet).getPropertyValue('bottom')
+          window.getComputedStyle(bullet).getPropertyValue("bottom")
         );
         if (
           obstacleLeft < bulletLeft &&
@@ -67,17 +67,21 @@ document.addEventListener('keydown', function (event) {
           bulletBottom >= obstacleBottom &&
           bulletBottom <= obstacleBottom + 25
         ) {
-          obstacle.style.left = window.innerWidth + 'px';
+          obstacle.style.left = window.innerWidth + "px";
           //score += 200; //총알에 맞으면 장애물이 맞을 시 점수 올리기
           scoreDisplay.innerText = score;
-          obstacleSpeed+=0.2;
+          obstacleSpeed += 0.2;
           clearInterval(bulletInterval);
-          bullet.style.left = '-30px';
+          bullet.style.left = "-30px";
           shooting = false;
-        } else if (obstacleLeft < 50 && obstacleLeft > 0 && characterTop >= 150) {
+        } else if (
+          obstacleLeft < 50 &&
+          obstacleLeft > 0 &&
+          characterTop >= 150
+        ) {
           gameOver = true;
-          gameOverDisplay.innerText = '게임 끝!! 점수: ' + score;
-          restartButton.style.display = 'block';
+          gameOverDisplay.innerText = "게임 끝!! 점수: " + score;
+          restartButton.style.display = "block";
         }
       }
     }, 10);
@@ -86,65 +90,56 @@ document.addEventListener('keydown', function (event) {
   }
 });
 
-
-
-
-
 function moveObstacle() {
-    var obstacleLeft = parseInt(
-      window.getComputedStyle(obstacle).getPropertyValue('left')
-    );
-  
-    if (obstacleLeft > -60) {
-      obstacle.style.left = obstacleLeft - obstacleSpeed + 'px';
-    } else {
-      obstacle.style.left = window.innerWidth + 'px';
-      score+=100;
-      scoreDisplay.innerText = score;
-    }
-  }
+  var obstacleLeft = parseInt(
+    window.getComputedStyle(obstacle).getPropertyValue("left")
+  );
 
-  var game = document.getElementById('game');
+  if (obstacleLeft > -60) {
+    obstacle.style.left = obstacleLeft - obstacleSpeed + "px";
+  } else {
+    obstacle.style.left = window.innerWidth + "px";
+    score += 100;
+    scoreDisplay.innerText = score;
+  }
+}
+
+var game = document.getElementById("game");
 var backgroundPosition = 0;
 var backgroundSpeed = 2;
 
 function moveBackground() {
   backgroundPosition -= backgroundSpeed;
-  game.style.backgroundPosition = backgroundPosition + 'px 0';
+  game.style.backgroundPosition = backgroundPosition + "px 0";
 }
 
 setInterval(moveBackground, 10);
 
-  
-
 setInterval(function () {
-    if (gameOver) return;
-  
-    var characterTop = parseInt(
-      window.getComputedStyle(character).getPropertyValue('top')
-    );
-    var obstacleLeft = parseInt(
-      window.getComputedStyle(obstacle).getPropertyValue('left')
-    );
-  
-    if (obstacleLeft < 50 && obstacleLeft > 0 && characterTop >= 150) {
-      // Game Over
-      gameOver = true;
-      gameOverDisplay.innerText = 'Do`h!!';
-      restartButton.style.display = 'block';
-    }
-  
-    moveObstacle();
-  
-  }, 10);
-  
-  
-  var restartButton = document.createElement('button');
-  restartButton.id = 'restart-button';
-  restartButton.innerText = '시작 화면으로';
-  restartButton.addEventListener('click', function () {
-    location.reload();
-  });
-  restartButton.style.display = 'none';
-  document.getElementById('game').appendChild(restartButton);
-  
+  if (gameOver) return;
+
+  var characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  var obstacleLeft = parseInt(
+    window.getComputedStyle(obstacle).getPropertyValue("left")
+  );
+
+  if (obstacleLeft < 50 && obstacleLeft > 0 && characterTop >= 150) {
+    // Game Over
+    gameOver = true;
+    gameOverDisplay.innerText = "Do`h!!";
+    restartButton.style.display = "block";
+  }
+
+  moveObstacle();
+}, 10);
+
+var restartButton = document.createElement("button");
+restartButton.id = "restart-button";
+restartButton.innerText = "시작 화면으로";
+restartButton.addEventListener("click", function () {
+  location.reload();
+});
+restartButton.style.display = "none";
+document.getElementById("game").appendChild(restartButton);
